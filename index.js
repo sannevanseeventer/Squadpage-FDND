08fca53f99c79a45500c40b96a01aa9f69dbd0b3
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-02-23 21:37:30 +0100

    Node en EJS comments toegevoegd

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 import express from 'express'
 
+// Deze link staat in verbiding met de database van alle squads
 const url = 'https://whois.fdnd.nl/api/v1/squad/'
 
 // Maak een nieuwe express app
 const app = express()
 
 // Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.set('view engine', 'ejs') // verwijst naar de htmlcode
+app.set('views', './views') // verwijst naar de footer, header en de index
+app.use(express.static('public')) // verwijst naar alle zichtbare onderdelen in de public map
 
-// Maak een route voor de index
-app.get('/', (request, response) => {
-  console.log(request.query.squad)
+// Maakt een route voor de index, get = informate die je terugkrijgt vanuit de database
+app.get('/', (request, response) => { // Dit zorg ervoor dat je de informatie ontvangt. Je vraagt en krijgteen antwoord terug
+  console.log(request.query.squad) // test met de onderstaande slug squad-a-2022
 
-  let slug = request.query.squad || 'squad-a-2022'
-  let orderBy = request.query.orderBy || 'name'
-  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+  let slug = request.query.squad || 'squad-a-2022' // Deze squad is standaard zichtbaar bij een request
+  let orderBy = request.query.orderBy || 'name' // Voor als je wilt sorteren op naam ipv op squad
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC' // bovenstaande url regel 4 + slug + orderby + volgorde voor naar achter
 
-  fetchJson(squadUrl).then((data) => {
+  fetchJson(squadUrl).then((data) => { // bij gebruik url -> zie je data -> vervolgens response met de index +  data
     response.render('index', data)
   })
 
 })
 
-// app.get('/members', (request, response) => {
-//   response.send('Joepie!!')
-// })
-
-// Stel het poortnummer in en start express
+// Stelt het poortnummer in en start express
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+  console.log(`Application started on http://localhost:${app.get('port')}`) // test met de localhost
 })
 
 /**
@@ -40,7 +37,7 @@ app.listen(app.get('port'), function () {
  * @returns the json response from the api endpoint
  */
 async function fetchJson(url) {
-  return await fetch(url)
+  return await fetch(url) // await zorgt ervoor dat er wordt gewacht voordat alle data beschikbaar is
     .then((response) => response.json())
     .catch((error) => error)
 }
